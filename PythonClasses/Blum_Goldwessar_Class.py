0bd207d1cf548af586a503f01932bdceea0118e5
uone <qizhu8@126.com>
2018-12-02 17:26:12 -0500

    local test version without any attack

--- PythonClasses/Blum_Goldwessar_Class.py
+++ PythonClasses/Blum_Goldwessar_Class.py
@@ -45,9 +45,6 @@ class BG(object):
         else:
             raise Exception("n should be a blum integer")
 
-    def get_public_key(self):
-        return [self.n]
-
     def update_n(self):
         if self.p != -1 and self.q != -1:
             self.n = self.p * self.q
@@ -62,30 +59,39 @@ class BG(object):
             raise Exception("please use set_n() to initialize n with a blum integer")
 
     def encrypt(self, m, x0=-1):
+        if isinstance(m, int):
+            m = bin(m)
+
         if self.n == -1:
             raise Exception("Please use set_n() to set n first")
 
         if x0 == -1:
             x0_sqrt = np.random.randint(self.n)
             x0 = x0_sqrt*x0_sqrt % self.n
+            print("BG chooses x0:", x0)
         t = np.ceil(len(m)/self.h)
 
         m_group, ciphertext = [], []
         for i in range(0, len(m), self.h):
-            m_group.append(m[i:i+4])
+            m_group.append(m[i:i+self.h])
         if len(m_group[-1]) < self.h:
             m_group[-1] += "0" * (self.h - len(m_group[-1]))
 
         xi = x0
         for i, mi in enumerate(m_group):
-            xi = xi * xi % self.n
+            xi = pow(int(xi), 2, int(self.n))
+            # xi = xi * xi % self.n
             pi = xi & self.h_mask
             ci = int(mi, 2) ^ pi
             ciphertext.append(ci)
         ciphertext.append(xi)
+        ciphertext = '|'.join([str(num) for num in ciphertext])
         return ciphertext
 
-    def decrypt(self, c):
+    def decrypt(self, c, bin_on=True):
+        if isinstance(c, str):
+            c = [int(item) for item in c.split('|')]
+
         if self.p == -1 or self.q == -1:
             raise Exception("Please use set_p() or set_q() to initialize p and q")
 
@@ -99,22 +105,41 @@ class BG(object):
         p_inv = npkg.mult_inv_mod_N(self.p, self.q)
         q_inv = npkg.mult_inv_mod_N(self.q, self.p)
 
-        xi = (self.p * p_inv * rq + self.q * q_inv * rp) % self.n
-
+        xi = (((self.p * p_inv)% self.n) * rq) % self.n + (((self.q * q_inv) %self.n) * rp) % self.n
+        xi %= self.n
 
         m_dec = ""
         for i, ci in enumerate(c[:-1]):
-            xi = xi * xi % self.n
+            # xi = xi * xi % self.n
+            xi = pow(int(xi), 2, int(self.n))
             pi = xi & self.h_mask
             mi = ci ^ pi
             mi_bin = bin(mi)[2:]
             mi_bin = "0"*(self.h - len(mi_bin)) + mi_bin
             m_dec += mi_bin
 
-        return m_dec
+        if bin_on:
+            print("BG dec rst:", m_dec)
+            if m_dec[-1] != '0':
+                return m_dec
+            for i in range(1, len(m_dec)):
+                if m_dec[-i] != '0':
+                    break
+            i -= 1
+            return m_dec[:-i]
+        else:
+            return int(m_dec, 2)
+
+
+    def get_public_key(self):
+        return [self.n]
+
+    def get_public_key_str(self):
+        self.update_n()
+        return str(self.n)
 
     def random_private_key(self):
-        p, q, n = npkg.blum_interger_generator(2**32, 2**10)
+        p, q, n = npkg.blum_interger_generator(2**21, 2**19)
         # r = np.random.randint(n)
         # x0 = r * r % n
         self.set_p(p)
--- my_test.py
+++ my_test.py
@@ -5,74 +5,143 @@ import time
 import numpy as np
 import PythonClasses.Number_Package as npkg
 from PythonClasses.RSA_Class import RSA
+from PythonClasses.DES_Class import DES
+
+from PythonClasses.Blum_Goldwessar_Class import BG
 from PythonClasses.SHA1_Class import SHA1
 import hashlib
 from PythonClasses.HMAC_Class import HMAC
 import hmac
 from User_Class import User
 
-user_info_db = User_Info_DB()
-user_info_db.add_user(user_id=135, ip="192.168.31.31")
-print("add a user twice")
-user_info_db.add_user(user_id=135, ip="192.168.31.32")
-print("check: number of users:", len(user_info_db.user_behave_db))
+def print_pkg_info(pkg_info):
+    for key in Constants.PKG_INFO_ITEMS:
+        if pkg_info[key]:
+            print(key, ":", pkg_info[key])
 
-user_info_db.add_user(user_id=133, ip="192.168.31.128")
-user_info_db.add_record(user_id=133, behavior="DoS_ATK")
-print(user_info_db.check_user(user_id=133))
+# user_info_db = User_Info_DB()
+# user_info_db.add_user(user_id=135, ip="192.168.31.31")
+# print("add a user twice")
+# user_info_db.add_user(user_id=135, ip="192.168.31.32")
+# print("check: number of users:", len(user_info_db.user_behave_db))
+#
+# user_info_db.add_user(user_id=133, ip="192.168.31.128")
+# user_info_db.add_record(user_id=133, behavior="DoS_ATK")
+# print(user_info_db.check_user(user_id=133))
+#
+# user_info_db.add_user(user_id=999, ip="192.168.31.128")
+#
+# print(user_info_db.check_ip("192.168.31.128"))
+# print(user_info_db.check_ip("192.168.31.31"))
+# print(user_info_db.check_ip("192.168.31.131")) # new ip
 
-user_info_db.add_user(user_id=999, ip="192.168.31.128")
+alice = User(user_id=1234)
+bob = User(user_id=4321)
 
-print(user_info_db.check_ip("192.168.31.128"))
-print(user_info_db.check_ip("192.168.31.31"))
-print(user_info_db.check_ip("192.168.31.131")) # new ip
+print("="*40)
+nego_choices = ['RSA', 'DES', 'SHA1']
+alice_HELLO_MSG_pkg_info = alice.HELLO_MSG_gen(nego_choices)
+alice_HELLO_MSG_pkg_info_interp = bob.pkg_interp(alice.pkg_gen(alice_HELLO_MSG_pkg_info))
+print_pkg_info(alice_HELLO_MSG_pkg_info_interp)
+bob.cur_dst_user_info['public_key_str']=alice.public_key_str
 
 
-#
-#
-# m = "abc"
-# S = SHA1()
-# hashed = S.hash(m.encode())
-# print(hashed)
-#
-#
-# hashlib_rst = hashlib.sha1(m.encode()).hexdigest()
-# print(hashed)
-#
-#
-#
-#
-# m, k = "123", "oqwiejrhaskdf"
-#
-# print(HMAC(m=m, k=k))
-#
-# print(hmac.new(k.encode('utf-8'), m.encode('utf-8'), hashlib.sha1).hexdigest())
-#
-# print(m)
-#
-#
-#
-# rsa = RSA()
-# print(rsa.get_public_key())
-#
-# c = rsa.sign(123123123123123123)
-# p = rsa.de_sign(c)
-# print(c, p)
-#
-# alice = User()
-#
-# print(alice.cert)
-# print(alice.public_key)
-# cert = "4563|1543692210.0468721|451628423787981108"
-# SRC_ID =4563
-# N, e = 4611686044196354491, 3152956804623902679
-# print(alice.cert_check(cert=cert, SRC_ID=SRC_ID, N=N, e=e))
-#
-# N, e = alice.public_key
-# print(alice.cert_check(cert=alice.cert, SRC_ID=alice.user_id, N=N, e=e))
-#
-# print(alice.SRC_ID_check("123asd"))
-#
+print("="*40)
+alice_ACK_CERT_pkg_info = alice.ACK_CERT_gen()
+alice_ACK_CERT_pkg_info_interp = bob.pkg_interp(alice.pkg_gen(alice_ACK_CERT_pkg_info))
+print_pkg_info(alice_ACK_CERT_pkg_info_interp)
+alice.cur_dst_user_info['public_key_str']=bob.public_key_str
 
-p, q, n = npkg.blum_interger_generator(2**32, 2**10)
-print(p, q, n)
+print("="*40)
+alice_DNY_MSG_pkg_info = alice.DNY_MSG_gen(1)
+alice_DNY_MSG_pkg_info_interp = bob.pkg_interp(alice.pkg_gen(alice_DNY_MSG_pkg_info))
+print_pkg_info(alice_DNY_MSG_pkg_info_interp)
+
+# print("="*40)
+# alice_CERT_REQ_pkg_info = alice.CERT_REQ_gen()
+# alice_CERT_REQ_pkg_info_interp = bob.pkg_interp(alice.pkg_gen(alice_CERT_REQ_pkg_info))
+# print_pkg_info(alice_CERT_REQ_pkg_info_interp)
+
+print("="*40, "CERT_RPY")
+alice_CERT_RPY_pkg_info = alice.CERT_RPY_gen()
+alice_CERT_RPY_pkg_info_interp = bob.pkg_interp(alice.pkg_gen(alice_CERT_RPY_pkg_info))
+print_pkg_info(alice_CERT_RPY_pkg_info_interp)
+
+print("="*40)
+alice_CERT_ERR_pkg_info = alice.CERT_ERR_gen(3)
+alice_CERT_ERR_pkg_info_interp = bob.pkg_interp(alice.pkg_gen(alice_CERT_ERR_pkg_info))
+print_pkg_info(alice_CERT_ERR_pkg_info_interp)
+
+print("="*40)
+alice.PKC_obj = BG()
+# alice.PKC_obj = RSA()
+
+alice.PKC_obj.random_private_key()
+alice_KEY_REQ_pkg_info = alice.KEY_REQ_gen()
+alice_KEY_REQ_pkg_info_interp = bob.pkg_interp(alice.pkg_gen(alice_KEY_REQ_pkg_info))
+print_pkg_info(alice_KEY_REQ_pkg_info_interp)
+
+print("="*40)
+bob.PKC_obj = BG()
+bob.PKC_obj.set_n(int(alice_KEY_REQ_pkg_info_interp['KEY_INFO']))
+# bob.PKC_obj.set_p(alice.PKC_obj.p)
+# bob.PKC_obj.set_q(alice.PKC_obj.q)
+
+# bob.PKC_obj = RSA()
+# bob.PKC_obj.set_e_N(alice.PKC_obj.e, alice.PKC_obj.N)
+
+bob.SymmEnc_obj = DES()
+bob.SymmEnc_obj.random_key()
+bob_KEY_RPY_info = bob.KEY_RPY_gen()
+bob_KEY_RPY_info_interp = alice.pkg_interp(bob.pkg_gen(bob_KEY_RPY_info))
+print_pkg_info(bob_KEY_RPY_info_interp)
+
+print("="*40)
+alice.SymmEnc_obj = DES()
+key_with_padding = alice.PKC_obj.decrypt(bob_KEY_RPY_info_interp['KEY_INFO'], bin_on=True)
+print(key_with_padding, "@@@@", len(key_with_padding))
+print("str 2 change:", key_with_padding[:-19])
+alice.SymmEnc_obj.init_key=int(key_with_padding[:-19], 2)
+print("bob send:", bob.SymmEnc_obj.init_key)
+print("alice get:", alice.SymmEnc_obj.init_key)
+
+bob_KEY_ERR_info = bob.KEY_ERR_gen()
+bob_KEY_ERR_info_interp = alice.pkg_interp(bob.pkg_gen(bob_KEY_ERR_info))
+print_pkg_info(bob_KEY_ERR_info_interp)
+
+print("="*40 + "MSG")
+bob_COM_MSG_info = bob.COM_MSG_gen("hello")
+print_pkg_info(bob_COM_MSG_info)
+print("="*40 + "MSG")
+bob_COM_MSG_info_interp = alice.pkg_interp(bob.pkg_gen(bob_COM_MSG_info))
+print_pkg_info(bob_COM_MSG_info_interp)
+print(alice.SymmEnc_obj.decrypt(bob_COM_MSG_info_interp['PAYLOAD']))
+
+
+print("="*40 + "MSG")
+bob_COM_ERR_info = bob.COM_ERR_gen(1)
+bob_COM_ERR_info_interp = alice.pkg_interp(bob.pkg_gen(bob_COM_ERR_info))
+print_pkg_info(bob_COM_ERR_info_interp)
+
+print("="*40)
+bob_DISCON_REQ_info = bob.DISCON_REQ_gen()
+bob_DISCON_REQ_info_interp = alice.pkg_interp(bob.pkg_gen(bob_DISCON_REQ_info))
+print_pkg_info(bob_DISCON_REQ_info_interp)
+
+print("="*40)
+bob_DISCON_CLG_info = bob.DISCON_CLG_gen()
+bob_DISCON_CLG_info_interp = alice.pkg_interp(bob.pkg_gen(bob_DISCON_CLG_info))
+print_pkg_info(bob_DISCON_CLG_info_interp)
+
+print("="*40)
+bob_DISCON_RPY_info = alice.DISCON_RPY_gen(bob_DISCON_CLG_info_interp["CHALLG"])
+bob_DISCON_RPY_info_interp = bob.pkg_interp(alice.pkg_gen(bob_DISCON_RPY_info))
+print_pkg_info(bob_DISCON_RPY_info_interp)
+
+bob.cur_dst_user_info['public_key'] = alice.sign_obj.get_public_key()
+print(bob.CHALLG_check(bob_DISCON_RPY_info_interp['CHALLG_RPY']))
+
+print("="*40)
+bob_DISCON_ERR_info = bob.DISCON_ERR_gen(1)
+bob_DISCON_ERR_info_interp = alice.pkg_interp(bob.pkg_gen(bob_DISCON_ERR_info))
+print_pkg_info(bob_DISCON_ERR_info_interp)
